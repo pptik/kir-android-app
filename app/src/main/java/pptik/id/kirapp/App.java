ca41cef53c80d53fbd3fca2a2686df4842a8b8e1
hynra <hynra@yahoo.com>
2017-11-13 13:50:11 +0700

    override default app

---
+++ app/src/main/java/pptik/id/kirapp/App.java
@@ -0,0 +1,15 @@
+package pptik.id.kirapp;
+
+
+import android.app.Application;
+
+import com.androidnetworking.AndroidNetworking;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidNetworking.initialize(this);
+    }
+}
